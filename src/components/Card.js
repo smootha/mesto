488e9c88101850b2984f24dd99f89f6399fcc24b
Eugene Dovganich <passionuberalles@yandex.ru>
2022-11-21 07:32:42 +0500

    hotfix: Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,12 +7,12 @@ export class Card {
   }
 //Клонирование темплейта карточки
   _getTemplate() {
-    const cardElement = this._templateElement.content.querySelector('.cards__item').cloneNode(true);
+    const cardElement = document.querySelector('.cards__template').content.querySelector('.cards__item').cloneNode(true);
     return cardElement;
   }
 //Лайк
   _handleLikeClick() {
-    this._like.classList.toggle('cards__like_active');
+    this._newCard.querySelector('.cards__like').classList.toggle('cards__like_active');
   }
 //Удаление
   _handleDeleteClick() {
@@ -33,12 +33,9 @@ export class Card {
 //Создание карточки
   generateCard() {
     this._newCard = this._getTemplate();
-    this._like = this._newCard.querySelector('.cards__like');
-    this._delete = this._newCard.querySelector('.cards__delete');
     this._newCard.querySelector('.cards__name').textContent = this._name;
-    this._cardImage = this._newCard.querySelector('.cards__image');
-    this._cardImage.alt = this._name;
-    this._cardImage.src = this._image;
+    this._newCard.querySelector('.cards__image').alt = this._name;
+    this._newCard.querySelector('.cards__image').src = this._image;
     this._setEventListeners();
     return this._newCard;
   }
